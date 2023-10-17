e33554dad42efd353d05b8ac584b2766e619da48
Steven-Klavins <stevenklavins94@gmail.com>
2023-10-17 15:42:51 +0100

    added Author seeds for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Author.create!(
+  first_name: "Christopher",
+  last_name: "Paolini",
+  dob: Date.new(1983,11,17),
+  is_alive: true
+)
+
+Author.create!(
+  first_name: "George",
+  last_name: "Orwell",
+  dob: Date.new(1903,06,25),
+  is_alive: false
+)
+
