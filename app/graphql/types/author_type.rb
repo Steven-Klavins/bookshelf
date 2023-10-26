92bfc46d242d89d649fa44cf77f00e5693f43f6c
Steven-Klavins <stevenklavins94@gmail.com>
2023-10-26 20:53:30 +0100

    added full name to author type

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -3,8 +3,14 @@ class Types::AuthorType < Types::BaseObject
   description "A single author."
 
   field :id, ID, null: false
-  field :first_name, String, null: true
-  field :last_name, String, null: true
-  field :dob, Int, null: false
+  field :first_name, String, null: false
+  field :last_name, String, null: false
+  field :dob, GraphQL::Types::ISO8601Date, null: true
   field :is_alive, Boolean, null: true
+  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :full_name, String, null: false
+
+  def full_name
+    "#{object.first_name} #{object.last_name}"
+  end
 end
