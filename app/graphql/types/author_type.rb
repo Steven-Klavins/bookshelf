0b45be1fd056a32d8adf1480b6e0afd6c7851a59
Steven-Klavins <stevenklavins94@gmail.com>
2023-10-27 18:51:03 +0100

    added error field to author type

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -9,16 +9,22 @@ class Types::AuthorType < Types::BaseObject
   field :is_alive, Boolean, null: true
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   field :full_name, String, null: false
+  field :errors, [Types::ErrorType], null: true
 
   def full_name
     "#{object.first_name} #{object.last_name}"
+  end
+
+  def errors
+    object.errors.map {|e| {field_name: e.attribute, error: object.errors[e.attribute]}}
   end
 end
 
 class Types::AuthorInputType < GraphQL::Schema::InputObject
   graphql_name "AuthorInputType"
-  description "The attributes required to create an author."
+  description "The attributes required to create/update an author."
 
+  argument :id, ID, required: false
   argument :first_name, String, required: true
   argument :last_name, String, required: true
   argument :dob, GraphQL::Types::ISO8601Date, required: false
